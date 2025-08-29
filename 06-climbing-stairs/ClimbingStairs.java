f6f5ac8aa5a55a67fa197daf8a69ee1462aa6db1
ViniciusMCabral <vmachakcabral@gmail.com>
2025-08-29 20:53:36 -0300

    Climbing Stairs

---
+++ 06-climbing-stairs/ClimbingStairs.java
@@ -0,0 +1,26 @@
+
+public class ClimbingStairs {
+
+	public static void main(String[] args) {
+		Integer resultado1 = climbingStairs(2);
+		Integer resultado1 = climbingStairs(3);
+		System.out.println(resultado1);
+		System.out.println(resultado2);
+
+	}
+
+	public static Integer climbingStairs(Integer n) {
+		if (n <= 2) {
+			return n;
+		}
+		
+		int a = 1, b = 2;
+		
+		for (int i = 3; i <= n; i++) {
+			int temp = a + b;
+			a = b;
+			b = temp;
+		}
+		return b;
+	}
+}
